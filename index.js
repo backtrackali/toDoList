b5e7faa90127fc33a7dc8e25a166fe23efeb0429
Grafirenato <grafirenato@gmail.com>
2025-03-13 23:57:34 -0400

    Código com MVC

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
-const express = require('express')
-const app = express()
+const express = require("express");
+const path = require("path");
+const routes = require("./routes/routes");
 
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
+const app = express();
+const port = 3000;
 
-app.listen(3000)
+app.set("view engine", "ejs");
+app.use(express.static(path.join(__dirname, "public")));
+app.use(routes);
+
+app.listen(port, () => {
+  console.log(`Servidor rodando em http://localhost:${port}`);
+});
